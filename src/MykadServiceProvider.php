f4b3a7f72ba78514d79f2aa5d541aa1ccba0e0b1
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-04-10 19:25:18 +0800

    Initial commit for package skeleton.

---
+++ src/MykadServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TelcoLAB\MyKad;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyKadServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
