267a3e849c33c786d03d4585ca25be0caffa6206
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-04-13 02:29:41 +0800

    Add extender for the methods.

--- src/MykadServiceProvider.php
+++ src/MykadServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace TelcoLAB\MyKad;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use TelcoLAB\MyKad\Validations\Validator;
 
 class MyKadServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,7 @@ class MyKadServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->extendValidation(Validator::class);
     }
 
     /**
@@ -24,5 +27,25 @@ class MyKadServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    public function extendValidation($class)
+    {
+        $classes = is_array($class) ? $class : [$class];
+
+        foreach ($classes as $class) {
+            if (class_exists($class) && $methods = get_class_methods($class)) {
+                $validations = Collection::make($methods)->reduce(function ($filtered, $item) {
+                    if (Str::startsWith($item, 'validate')) {
+                        $filtered[$item] = Str::snake($strip = str_replace('validate', '', $item));
+                    }
+                    return $filtered;
+                });
+
+                foreach ($validations as $methodName => $validationName) {
+                    $this->app->validator->extend($validationName, $class . '@' . $methodName);
+                }
+            }
+        }
     }
 }
