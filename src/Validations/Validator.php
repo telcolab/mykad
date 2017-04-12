0ec5878029c4d731bbc94413eb89dd065ad817a0
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-04-13 02:24:06 +0800

    Add custom validations.

---
+++ src/Validations/Validator.php
@@ -0,0 +1,31 @@
+<?php
+namespace TelcoLAB\MyKad\Validations;
+
+use TelcoLAB\MyKad\Facades\MyKad;
+
+class Validator
+{
+    public function validateMykadCheck($attribute, $value, $parameters)
+    {
+        $mykad = MyKad::parse($value, false);
+
+        return $mykad->isValidMyKad();
+    }
+
+    public function validateMykadIsOver($attribute, $value, $parameters)
+    {
+        $mykad = MyKad::parse($value, false);
+
+        return $mykad->isOver($parameters[0]);
+    }
+
+    public function validateMykadIsOver12($attribute, $value, $parameters)
+    {
+        return $this->validateMykadIsOver($attribute, $value, [12]);
+    }
+
+    public function validateMykadIsOver18($attribute, $value, $parameters)
+    {
+        return $this->validateMykadIsOver($attribute, $value, [18]);
+    }
+}
