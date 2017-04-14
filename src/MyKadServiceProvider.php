ec1522d81b94e8926b6855ed8f65bdf6c61d0b6d
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-04-14 14:13:04 +0800

    Use Extender package to handle extening validations.

--- src/MyKadServiceProvider.php
+++ src/MyKadServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace TelcoLAB\MyKad;
 
-use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use TelcoLAB\Extender\Extender;
 use TelcoLAB\MyKad\Validations\Validator;
 
 class MyKadServiceProvider extends ServiceProvider
@@ -16,8 +16,9 @@ class MyKadServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadTranslationsFrom(__DIR__ . '/assets/lang', 'mykad');
-        $this->extendValidation(Validator::class);
+        $this->loadTranslationsFrom(__DIR__ . '/assets/lang', 'extender');
+        $extender = (new Extender)->make(Validator::class);
+        $extender->extend();
     }
 
     /**
@@ -28,26 +29,5 @@ class MyKadServiceProvider extends ServiceProvider
     public function register()
     {
         //
-    }
-
-    public function extendValidation($class)
-    {
-        $classes = is_array($class) ? $class : [$class];
-
-        foreach ($classes as $class) {
-            if (class_exists($class) && $methods = get_class_methods($class)) {
-                $validations = Collection::make($methods)->reduce(function ($filtered, $item) {
-                    if (Str::startsWith($item, 'validate')) {
-                        $filtered[$item] = Str::snake($strip = str_replace('validate', '', $item));
-                    }
-                    return $filtered;
-                });
-
-                foreach ($validations as $methodName => $validationName) {
-                    $message = $this->app->translator->trans('mykad::validation.' . $validationName);
-                    $this->app->validator->extend($validationName, $class . '@' . $methodName, $message);
-                }
-            }
-        }
     }
 }
